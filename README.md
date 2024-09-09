7a8233e5e6fc2c53ad87b9bb60e83f8542d57d1c
Rodrigo Suárez <rodrigosrz12@gmail.com>
2024-09-10 00:47:39 +0200

    test the model and the chat from chatgpt

---
+++ README.md
@@ -0,0 +1,19 @@
+## How to run
+
+Create a virtual environment:
+
+```bash
+python -m venv .venv
+```
+
+Install the dependencies:
+
+```bash
+uv install
+```
+
+Run the app:
+
+```bash
+python hello.py
+```
--- hello.py
+++ hello.py
@@ -1,25 +1,55 @@
 import os
 from dotenv import load_dotenv, find_dotenv
-from langchain_openai import ChatOpenAI
+from langchain_openai import OpenAI, ChatOpenAI
 
 _ = load_dotenv(find_dotenv())
 
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_ORGANIZATION = os.getenv("OPENAI_ORGANIZATION")
 
-def main():
-    llm = ChatOpenAI(
-        model="gpt-4o-mini",
+
+def chatgpt_api():
+    llm = OpenAI(
+        model="gpt-3.5-turbo-instruct",
         organization=OPENAI_ORGANIZATION,
         api_key=OPENAI_API_KEY,
+        temperature=0.1,  # how creative the LLM is
     )
     print("Calling the LLM...")
     try:
         response = llm.invoke("Say hi to Rodrigo")
-        print(response.content)
+        print(response)
     except Exception as e:
         print(f"Error: {e}")
 
+    try:
+        print("Calling the LLM in streaming mode...")
+        for chunk in llm.stream("Say hi to Rodrigo"):
+            print(chunk, end="", flush=True)
+    except Exception as e:
+        print(f"Error: {e}")
+
+def chatgpt_chat():
+    llm = ChatOpenAI(
+        model="gpt-3.5-turbo-0125",
+        organization=OPENAI_ORGANIZATION,
+        api_key=OPENAI_API_KEY,
+    )
+    
+    messages = [
+        {"role": "system", "content": "You are an expert in python programming and you are tasked to help me with my code."},
+        {"role": "human", "content": "How is the best way to learn python architecture as a professional developer from a big tech company?"},
+    ]
+    try:
+        # response = llm.invoke(messages)
+        for chunk in llm.stream(messages):
+            print(chunk.content, end="", flush=True)
+    except Exception as e:
+        print(f"Error: {e}")
+
+def main():
+    # chatgpt_api()
+    chatgpt_chat()
 
 
 if __name__ == "__main__":
