1c11359747a4395d2ee581b208d0e68e816b9c32
Rodrigo Suárez <rodrigosrz12@gmail.com>
2024-09-24 08:51:56 +0200

    memory with summary

--- src/008-memory.py
+++ src/008-memory.py
@@ -2,8 +2,8 @@ import os
 
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
-from langchain.memory import ConversationBufferMemory
-from langchain.chains import LLMChain
+from langchain.memory import ConversationBufferMemory, ConversationSummaryBufferMemory
+from langchain.chains import LLMChain, ConversationChain
 from langchain_core.prompts import (
     ChatPromptTemplate,
     MessagesPlaceholder,
@@ -30,24 +30,26 @@ def main():
     
     # Correct way to do it ----------------------------
     prompt = ChatPromptTemplate(
-    messages=[
-        SystemMessagePromptTemplate.from_template(
-            "You are a nice chatbot having a conversation with a human."
-        ),
-        MessagesPlaceholder(variable_name="chat_history"),
-            HumanMessagePromptTemplate.from_template("{user_input}")
-        ]
+        messages=[
+            SystemMessagePromptTemplate.from_template(
+                "You are a nice chatbot having a conversation with a human."
+            ),
+            MessagesPlaceholder(variable_name="chat_history"),
+                HumanMessagePromptTemplate.from_template("{user_input}")
+            ]
     )
     # -------------------------------------------   
     
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-
-    chain = LLMChain(llm=llm, prompt=prompt, memory=memory, verbose=True)
+    # memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+    # chain = LLMChain(llm=llm, prompt=prompt, memory=memory, verbose=True)
+    
+    buffer_memory = ConversationSummaryBufferMemory(memory_key="history", llm=llm, max_token_limit=100)
+    chain = ConversationChain(llm=llm, memory=buffer_memory, verbose=True)
 
     while True:
         user_input = input("You: ")
-        response = chain({"user_input": user_input})
-        print("Assistant: ", response["text"])
+        response = chain({"input": user_input})
+        print("Assistant: ", response)
 
 
 if __name__ == "__main__":
